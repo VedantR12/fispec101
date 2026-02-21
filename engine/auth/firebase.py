6b77e08118f9a359e55681e1ad470f27ee10c752
VedantR12 <vedant.rode.2005@gmail.com>
2026-02-21 12:53:59 +0530

    update firebase for cloud

--- engine/auth/firebase.py
+++ engine/auth/firebase.py
@@ -1,16 +1,16 @@
 import os
+import json
 import firebase_admin
 from firebase_admin import credentials
 
-
 def initialize_firebase():
     if not firebase_admin._apps:
-        service_account_path = os.getenv("FIREBASE_SERVICE_ACCOUNT_PATH")
+        firebase_json = os.getenv("FIREBASE_CREDENTIALS_JSON")
 
-        if not service_account_path:
-            raise RuntimeError(
-                "FIREBASE_SERVICE_ACCOUNT_PATH environment variable not set"
-            )
+        if not firebase_json:
+            raise Exception("Firebase credentials not set.")
 
-        cred = credentials.Certificate(service_account_path)
+        cred_dict = json.loads(firebase_json)
+        cred = credentials.Certificate(cred_dict)
+
         firebase_admin.initialize_app(cred)
